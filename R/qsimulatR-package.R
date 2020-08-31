57fa9891d621bd45787c7f8933dc6c763b638016
Carsten Urbach <curbach@gmx.de>
2020-08-31 18:36:09 +0200

    docu

---
+++ R/qsimulatR-package.R
@@ -0,0 +1,15 @@
+#' The qsimulatR Package
+#'
+#' A simulator for a quantum computer
+#'
+#' A quantum computer simulator framework. It currently implements the
+#' most common gates (X, Y, Z, H, Z, S, T, Rz, CNOT), supports plotting
+#' of circuits and is able to export circuits into IBM's QASM python
+#' language.
+#'
+#' @name qsimulatR
+#' @docType package
+#' @author Carsten Urbach, \email{urbach@hiskp.uni-bonn.de}
+#'
+#' @keywords package
+NULL
