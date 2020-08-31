45b66c9ad5028fb5277c02b8d6d4ed44881aa08c
Carsten Urbach <curbach@gmx.de>
2020-08-31 22:48:01 +0200

    general controlled qgates

--- R/state.R
+++ R/state.R
@@ -312,7 +312,6 @@ setMethod("*", c("sqgate", "qstate"),
             stopifnot(e1@bit > 0 && e1@bit <= e2@nbits)
             bit <- e1@bit
             nbits <- e2@nbits
-            ii <- c(0, 2^(bit-1))
             res <- c()
             al <- 0:(2^e2@nbits-1)
             ii <- is.bitset(al, bit=bit)
@@ -465,6 +464,73 @@ Y <- function(bit) {
 Z <- function(bit) {
   return(methods::new("sqgate", bit=as.integer(bit), M=array(as.complex(c(1., 0., 0., -1.)), dim=c(2,2)), type="Z"))
 }
+
+#' A controlled single qubit gate
+#'
+#' This class represents a generic single qubit gate
+#'
+#' @slot bits Integer. Integer vector of bits. The first is the control bit, the second the target bit.
+#' @slot gate sqgate. The single qubit gate.
+#'
+#' @details
+#' The qubits are counted from 1 to \code{nbits} starting with the least
+#' significant bit.
+#' 
+#' @examples
+#' x <- qstate(nbits=2)
+#' ## application of the X (NOT) gate to bit 1
+#' #z <- sqgate(bit=1L, M=array(as.complex(c(0,1,1,0)), dim=c(2,2))) * x
+#' #z
+#' 
+#' @name cqgate
+#' @rdname cqgate
+#' @aliases cqgate-class
+#' @exportClass cqgate
+setClass("cqgate",
+         representation(bits="integer",
+                        gate="sqgate"),
+         prototype(bits=c(1L, 2L),
+                   gate=Id(2L))
+         )
+                                        #         validity=check_sqgate)
+
+#' @export
+cqgate <- function(bits=c(1L, 2L), gate=Id(2L)) {
+  return(methods::new("cqgate", bits=as.integer(bits), gate=gate))
+}
+
+#' times-cqgate-qstate
+#'
+#' Applies a controlled single qubit gate to a quantum state.
+#'
+#' @param e1 object of S4 class 'cqgate'
+#' @param e2 object of S4 class 'qstate'
+#' @return
+#' An object of S4 class 'qstate'
+#'
+setMethod("*", c("cqgate", "qstate"),
+          function(e1, e2) {
+            stopifnot(all(e1@bits > 0) && all(e1@bits <= e2@nbits))
+            bits <- e1@bits
+            nbits <- e2@nbits
+            res <- e2@coefs
+            al <- 0:(2^e2@nbits-1)
+            cb  <- is.bitset(al, bit=bits[1])
+            ii <- is.bitset(al, bit=bits[2]) & cb
+            kk <- (!is.bitset(al, bit=bits[2])) & cb
+            res[kk] <- e1@gate@M[1,1]*e2@coefs[kk] + e1@gate@M[1,2]*e2@coefs[ii]
+            res[ii] <- e1@gate@M[2,1]*e2@coefs[kk] + e1@gate@M[2,2]*e2@coefs[ii]
+            ## the gatelist needs to be extended for plotting
+            circuit <- e2@circuit
+            ngates <- length(circuit$gatelist)
+            circuit$gatelist[[ngates+1]] <- list(type=e1@gate@type, bits=c(e1@bits, NA))
+            if(e1@gate@type == "Rz") circuit$gatelist[[ngates+1]]$angle <- -Re(2*1i*log(e1@M[2,2]))
+            circuit$gatelist[[ngates+1]]$controlled <- TRUE
+            
+            return(qstate(nbits=nbits, coefs=as.complex(res), basis=e2@basis, circuit=circuit))
+          }
+          )
+
 
 #' The CNOT gate
 #'
