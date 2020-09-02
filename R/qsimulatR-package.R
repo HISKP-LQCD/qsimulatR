ecea37780d609704d7445f1fe2aacd99a31f3735
Carsten Urbach <curbach@gmx.de>
2020-09-02 21:51:22 +0200

    rename export2qasm to export2qiskit to reflect purpose better

--- R/qsimulatR-package.R
+++ R/qsimulatR-package.R
@@ -7,8 +7,8 @@
 #' For convenience, it currently directly provides
 #' most common gates (X, Y, Z, H, Z, S, T, Rz, CNOT, SWAP,
 #' toffoli or CCNOT, CSWAP). qsimulatR supports plotting
-#' of circuits and is able to export circuits into IBM's QASM python
-#' language.
+#' of circuits and is able to export circuits into IBM's Qiskit python
+#' language, which can be run on IBM's real quantum hardware.
 #'
 #' @name qsimulatR
 #' @docType package
